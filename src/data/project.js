e841ffcd4e2478a41341134f4d3e5b64e48ed855
kib09 <dslqoehf@gmail.com>
2025-04-30 15:07:21 +0900

    250430

--- src/data/project.js
+++ src/data/project.js
@@ -7,7 +7,14 @@ const projects = [
     role: "유저 정보 관련 기능 구현 , firebase 연동 , UI 디자인",
   },
   {
-    title: "날씨 조회 앱",
+    title: "videOn 영화 검색 사이트",
+    period: "2023.11 ~ 2023.12",
+    stack: ["Vue", "OpenWeather API", "CSS3"],
+    description: "날씨 데이터를 지역별로 시각화하여 보여주는 반응형 SPA",
+    role: "API 연동, 뷰 라우팅, 반응형 스타일링",
+  },
+  {
+    title: "kropp 피트니스 사이트",
     period: "2023.11 ~ 2023.12",
     stack: ["Vue", "OpenWeather API", "CSS3"],
     description: "날씨 데이터를 지역별로 시각화하여 보여주는 반응형 SPA",
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -4,7 +4,7 @@ import bearsImg from "../assets/bg-bears.png";
 function Home() {
   return (
     <div className="bg-neutral-600 min-h-screen text-white pt-10 flex justify-center items-center">
-      <section className="flex py-20 px-4 gap-8">
+      <section className="flex flex-col md:flex-row py-20 px-4 gap-8 items-center">
         {/* 기울기 카드 */}
         <TiltCard
           image={bearsImg}
@@ -12,12 +12,14 @@ function Home() {
         />
 
         {/* 텍스트 정보 */}
-        <div className="flex flex-col justify-center">
-          <div className="text-7xl font-bold mb-4  border-b-yellow-500 border-b-4 pb-2">
+        <div className="flex flex-col justify-center text-center md:text-left">
+          <div className="text-5xl md:text-7xl font-bold mb-4 border-b-yellow-500 border-b-4 pb-2">
             KIM IN BAE
           </div>
-          <div className="text-3xl mb-2">WEB Prontend Developer</div>
-          <div className="text-lg"></div>
+          <div className="text-2xl md:text-3xl mb-2">
+            WEB Frontend Developer
+          </div>
+          <div className="text-lg">책임감 있게 성장하는 개발자</div>
         </div>
       </section>
     </div>
