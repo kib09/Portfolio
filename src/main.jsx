617fda1ca0f902d0a894c964edbeecbb24bfedb6
kib09 <dslqoehf@gmail.com>
2025-10-30 23:48:08 +0900

    25-10-30 intro 삭제 및 파일 정리

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,8 @@ import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import Intro from "./pages/Intro.jsx";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 
 createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Intro />} />
-        <Route path="/home" element={<App />} />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
+        <App /> 
 );
